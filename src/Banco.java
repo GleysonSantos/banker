c5de54b88ebc51918869e3537ae1819ff3d359f6
Gleyson Santos <gleyson.software@gmail.com>
2019-10-12 00:36:18 -0300

    Implementando método sacar

--- src/Banco.java
+++ src/Banco.java
@@ -1,9 +1,11 @@
 public class Banco {
     public static void main(String[] args) {
         ContaCorrente conta = new ContaCorrente();
-        Cliente cliente = new Cliente();
-        conta.titular = cliente;
+        conta.titular = new Cliente();
 
         conta.depositar(100);
+
+        conta.sacar(50);
+
     }
 }
--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -4,13 +4,24 @@ public class ContaCorrente {
     double saldo;
     Cliente titular;
 
-    public void depositar(int valor){
+    public void depositar(double valor) {
         System.out.println("Saldo anterior: R$ "+saldo);
         saldo += valor;
         System.out.println("Depósito de: R$ "+valor);
         System.out.println("Saldo atual: R$ "+saldo);
     }
 
-
+    public boolean sacar(double valor) {
+        if (valor <= saldo){
+            System.out.println("Saldo antes do saque: R$ "+saldo);
+            saldo -= valor;
+            System.out.println("Valor do saque: R$ "+valor);
+            System.out.println("Valor restante de saldo: R$"+saldo);
+            return true;
+        } else {
+            System.out.println("Você não possui saldo suficiente!");
+            return false;
+        }
+    }
 
 }
